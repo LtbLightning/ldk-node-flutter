058329990398a94ecb488029e66b60dccdfac45b
kumulynja <b@kumuly.dev>
2023-05-13 13:22:30 -0500

    add comment to explain naming

--- rust/copy.sh
+++ rust/copy.sh
@@ -7,6 +7,7 @@
        mkdir -p ../android/src/main/jniLibs/armeabi-v7a
        mkdir -p ../android/src/main/jniLibs/x86
 
+       # Name the native lib 'libldknode' when copying to avoid naming conflicts with other native code libs build from rust
        cp target/aarch64-linux-android/release/librust.so  ../android/src/main/jniLibs/arm64-v8a/libldknode.so
        cp target/armv7-linux-androideabi/release/librust.so  ../android/src/main/jniLibs/armeabi-v7a/libldknode.so
        cp target/i686-linux-android/release/librust.so  ../android/src/main/jniLibs/x86/libldknode.so
