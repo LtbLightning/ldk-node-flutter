dbf2c3f389106f18a944ed65d89208ccfaf59e08
kumulynja <b@kumuly.dev>
2023-05-14 15:43:57 -0500

    change binary names to librust_ldk_node

--- rust/copy.sh
+++ rust/copy.sh
@@ -1,16 +1,16 @@
        if [ -d "../android/src/main/jniLibs" ]; then rm -r ../android/src/main/jniLibs
         fi
-        if [ -e "../ios/libldknode.a" ]; then rm ../ios/libldknode.a
+        if [ -e "../ios/librust_ldk_node.a" ]; then rm ../ios/librust_ldk_node.a
         fi
 
        mkdir -p ../android/src/main/jniLibs/arm64-v8a
        mkdir -p ../android/src/main/jniLibs/armeabi-v7a
        mkdir -p ../android/src/main/jniLibs/x86
 
-       # Name the native lib 'libldknode' when copying to avoid naming conflicts with other native code libs build from rust
-       cp target/aarch64-linux-android/release/librust.so  ../android/src/main/jniLibs/arm64-v8a/libldknode.so
-       cp target/armv7-linux-androideabi/release/librust.so  ../android/src/main/jniLibs/armeabi-v7a/libldknode.so
-       cp target/i686-linux-android/release/librust.so  ../android/src/main/jniLibs/x86/libldknode.so
+       # Name the native lib 'librust_ldk_node' when copying to avoid naming conflicts with other native code libs build from rust
+       cp target/aarch64-linux-android/release/librust.so  ../android/src/main/jniLibs/arm64-v8a/librust_ldk_node.so
+       cp target/armv7-linux-androideabi/release/librust.so  ../android/src/main/jniLibs/armeabi-v7a/librust_ldk_node.so
+       cp target/i686-linux-android/release/librust.so  ../android/src/main/jniLibs/x86/librust_ldk_node.so
 
-       cp target/universal/release/librust.a  ../ios/libldknode.a
+       cp target/universal/release/librust.a  ../ios/librust_ldk_node.a
 
