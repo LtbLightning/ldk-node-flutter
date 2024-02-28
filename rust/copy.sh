b3c863e776e49802afe5afc62ce0351ff3e85f54
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-02-27 19:20:00 -0500

    script files updated

--- rust/copy.sh
+++ rust/copy.sh
@@ -1,6 +1,6 @@
        if [ -d "../android/src/main/jniLibs" ]; then rm -r ../android/src/main/jniLibs
         fi
-        if [ -e "../ios/libldknode.a" ]; then rm ../ios/libldknode.a
+        if [ -d "../ios/ldk_node.xcframework" ]; then rm -r ../ios/ldk_node.xcframework
         fi
 
        mkdir -p ../android/src/main/jniLibs/arm64-v8a
@@ -8,10 +8,10 @@
        mkdir -p ../android/src/main/jniLibs/x86
        mkdir -p ../android/src/main/jniLibs/x86_64
 
-       cp target/aarch64-linux-android/release/libldknode.so  ../android/src/main/jniLibs/arm64-v8a
-       cp target/armv7-linux-androideabi/release/libldknode.so  ../android/src/main/jniLibs/armeabi-v7a
-       cp target/i686-linux-android/release/libldknode.so  ../android/src/main/jniLibs/x86
-       cp target/x86_64-linux-android/release/libldknode.so  ../android/src/main/jniLibs/x86_64
+       cp target/aarch64-linux-android/release/libldk_node.so  ../android/src/main/jniLibs/arm64-v8a
+       cp target/armv7-linux-androideabi/release/libldk_node.so  ../android/src/main/jniLibs/armeabi-v7a
+       cp target/i686-linux-android/release/libldk_node.so  ../android/src/main/jniLibs/x86
+       cp target/x86_64-linux-android/release/libldk_node.so  ../android/src/main/jniLibs/x86_64
 
-       cp target/universal/release/libldknode.a  ../ios/
+       cp -r ldk.0.2.1/ldk_node.xcframework  ../ios/
 
