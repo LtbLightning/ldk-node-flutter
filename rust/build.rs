503c700a71cbf07af0b361477751ad3a1fde2265
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-02-29 08:25:00 -0500

    frb build config updated

--- rust/build.rs
+++ rust/build.rs
@@ -1,19 +1,15 @@
-use std::env;
+use lib_flutter_rust_bridge_codegen::codegen;
+use lib_flutter_rust_bridge_codegen::codegen::Config;
+use lib_flutter_rust_bridge_codegen::utils::logs::configure_opinionated_logging;
 
-use lib_flutter_rust_bridge_codegen::{
-    config_parse, frb_codegen, get_symbols_if_no_duplicates, RawOpts,
-};
+fn main() -> anyhow::Result<()> {
+    // If you want to see logs
+    // Alternatively, use `cargo build -vvv` (instead of `cargo build`) to see logs on screen
+    configure_opinionated_logging("./logs/", true)?;
 
-fn main() {
-    env::set_var("RUST_BACKTRACE", "1");
-    // Options for frb_codegen are read from the config file
-    let raw_opts = RawOpts::try_parse_args_or_yaml().unwrap();
-
-    // get opts from raw opts
-    let all_configs = config_parse(raw_opts);
-
-    // generation of rust api for ffi (single block)
-    let all_symbols = get_symbols_if_no_duplicates(&all_configs).unwrap();
-    // assert_eq!(all_configs.len(), 1);
-    frb_codegen(&all_configs[0], &all_symbols).unwrap();
+    // Execute code generator with auto-detected config
+    codegen::generate(
+        Config::from_config_file("../flutter_rust_bridge.yaml")?.unwrap(),
+        Default::default(),
+    )
 }
