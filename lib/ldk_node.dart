78086d5aa9077467f46ce28ed31cf05080d98437
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-03-19 09:39:00 -0400

    code cleanup

--- lib/ldk_node.dart
+++ lib/ldk_node.dart
@@ -25,4 +25,4 @@ export './src/generated/api/types.dart'
         EntropySourceConfig_SeedFile;
 export 'src/root.dart';
 export 'src/utils/utils.dart'
-    hide LdkException, mapBuilderException, mapNodeException, Frb;
+    hide ExceptionBase, mapBuilderError, mapNodeBaseError, Frb;
