4c0952c93d62492627602b55b41598ba023101c2
kumulynja <b@kumuly.dev>
2024-04-06 12:44:54 +0200

    export U8Array64 and U8Array32

--- lib/ldk_node.dart
+++ lib/ldk_node.dart
@@ -24,5 +24,6 @@ export './src/generated/api/types.dart'
         GossipSourceConfig,
         EntropySourceConfig_SeedFile;
 export 'src/root.dart';
+export 'src/generated/api/node.dart' show U8Array64, U8Array32;
 export 'src/utils/utils.dart'
     hide ExceptionBase, mapBuilderError, mapNodeBaseError, Frb;
